3d9f3ac67eff423021cd788207a62faae502c1fd
Manuel Mansilla Gil <manuelmansillagil@MacBook-Pro-de-Manuel.local>
2023-01-26 11:01:03 +0100

    Psycopg2 installation

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,8 +7,8 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    """Test the calc module."""
 
+    """Test the calc module."""
 
     def test_add_numbers(self):
         """Test adding numbers together."""
@@ -21,3 +21,4 @@ class CalcTests(SimpleTestCase):
         res = calc.substract(10, 15)
 
         self.assertEqual(res, 5)
+
